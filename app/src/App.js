a1278c3a3719132e04d3f2b4aa312a4c2fc2de9e
lai2-24 <lai2-24@itismeucci.com>
2025-04-29 14:31:36 +0200

    i pulsanti incrementano lo stesso valore

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
 import { useState } from 'react';
 
-const [count, setCount] = useState(0);
-
-function handleClick() {
-  setCount(count + 1);
-}
-
-function MyButton() {
-
+function MyButton({ count, onClick }) {
   return (
-    <button onClick={handleClick}>
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
 }
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <h1>Counters that update at the same time</h1>
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
